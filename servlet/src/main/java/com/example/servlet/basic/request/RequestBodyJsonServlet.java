042589710754169ff3986a35a4e6aedd35399f73
hyun6ik <fkanwm@naver.com>
2021-03-09 23:55:59 +0900

    HTTP 요청 데이터 - API 메시지 바디 - JSON

---
+++ servlet/src/main/java/com/example/servlet/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,35 @@
+package com.example.servlet.basic.request;
+
+import com.example.servlet.basic.HelloData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper =  new ObjectMapper(); //JSON 결과를 파싱해서 사용할 수 있는 자바 객체로 변환 
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("JSON OK");
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println("helloData.getUsername() = " + helloData.getUsername());
+        System.out.println("helloData.getAge() = " + helloData.getAge());
+
+    }
+}
